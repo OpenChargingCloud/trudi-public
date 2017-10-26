d2a02cf71bcafedd2f6399606064e8621c671791
Thomas Müller <tmueller@ivugmbh.de>
2017-10-26 21:03:29 +0200

    Version 1.0.17: Handling von Server-IDs/Gerätekennungen die nicht nach DIN 43863-5 sind verbessert.

--- README.md
+++ README.md
@@ -26,8 +26,8 @@ Ansprechpartner:
 
 System           | Download
 ---              | ---
-Windows (64-Bit) | http://www.ivu-software.de/apps/TRuDI/TRuDI-Setup-1.0.14-x86_64.exe
-Windows (32-Bit) | http://www.ivu-software.de/apps/TRuDI/TRuDI-Setup-1.0.14-x86_32.exe
+Windows (64-Bit) | http://www.ivu-software.de/apps/TRuDI/TRuDI-Setup-1.0.17-x86_64.exe
+Windows (32-Bit) | http://www.ivu-software.de/apps/TRuDI/TRuDI-Setup-1.0.17-x86_32.exe
 Linux ([AppImage](https://de.wikipedia.org/wiki/AppImage), 64-Bit) | http://www.ivu-software.de/apps/TRuDI/TRuDI-1.0.14-x86_64.AppImage
 Linux (deb, 64-Bit) | http://www.ivu-software.de/apps/TRuDI/TRuDI-1.0.14_amd64.deb
 
--- src/TRuDI.Models.Tests/ServerIdTests.cs
+++ src/TRuDI.Models.Tests/ServerIdTests.cs
@@ -50,7 +50,7 @@ namespace TRuDI.Models.Tests
         }
 
         [TestMethod]
-        public void TestServerId4()
+        public void TestServerIdLowerCase()
         {
             var target = new ServerId("e ppc 02 10486901");
             Assert.AreEqual(ObisMedium.Communication, target.Medium);
@@ -61,6 +61,64 @@ namespace TRuDI.Models.Tests
             Assert.AreEqual("E PPC 02 10486901", target.ToString());
             Assert.AreEqual("EPPC0210486901", target.ToStringWithoutSpace());
             Assert.AreEqual("0A0E5050430200A00475", target.ToHexString());
+        }
+
+        [TestMethod]
+        public void TestServerIdInvalid()
+        {
+            var target = new ServerId("irgendwas");
+            Assert.IsFalse(target.IsValid);
+            Assert.AreEqual("irgendwas", target.ToString());
+            Assert.AreEqual("irgendwas", target.ToHexString());
+            Assert.AreEqual("irgendwas", target.ToStringWithoutSpace());
+        }
+
+        [TestMethod]
+        public void TestServerIdWiredMBus()
+        {
+            var target = new ServerId("0224010523AC484104");
+            Assert.IsTrue(target.IsValid);
+            Assert.AreEqual(ObisMedium.Heat, target.Medium);
+            Assert.AreEqual("REL", target.FlagId);
+            Assert.AreEqual(23050124u, target.Number);
+            Assert.AreEqual("REL 23050124", target.ToString());
+            Assert.AreEqual("0224010523AC484104", target.ToHexString());
+        }
+
+        [TestMethod]
+        public void TestServerIdWirelessMBus()
+        {
+            var target = new ServerId("01E230197600150003");
+            Assert.IsTrue(target.IsValid);
+            Assert.AreEqual(ObisMedium.Gas, target.Medium);
+            Assert.AreEqual("LGB", target.FlagId);
+            Assert.AreEqual(15007619u, target.Number);
+            Assert.AreEqual("LGB 15007619", target.ToString());
+            Assert.AreEqual("01E230197600150003", target.ToHexString());
+        }
+
+        [TestMethod]
+        public void TestServerIdDin_43863_5_2010_7()
+        {
+            var target = new ServerId("090E4d5543AB00bc614e");
+            Assert.IsTrue(target.IsValid);
+            Assert.AreEqual(ObisMedium.Communication, target.Medium);
+            Assert.AreEqual("MUC", target.FlagId);
+            Assert.AreEqual(12345678u, target.Number);
+            Assert.AreEqual("E MUC AB 12345678", target.ToString());
+            Assert.AreEqual("090E4D5543AB00BC614E", target.ToHexString());
+        }
+
+        [TestMethod]
+        public void TestServerIdDin_43863_5_2010_2()
+        {
+            var target = new ServerId("064d554301001dd8594E");
+            Assert.IsTrue(target.IsValid);
+            Assert.AreEqual(ObisMedium.Electricity, target.Medium);
+            Assert.AreEqual("MUC", target.FlagId);
+            Assert.AreEqual(12345678u, target.Number);
+            Assert.AreEqual("1 MUC 10 00 12345678", target.ToString());
+            Assert.AreEqual("064D554301001DD8594E", target.ToHexString());
         }
     }
 }
